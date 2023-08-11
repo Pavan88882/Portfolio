c3cd0a0ce227d3d0182c5ca18aa84f381b1c4426
Pavan88882 <pavanjagade90@gmail.com>
2023-08-11 14:14:51 +0530

    skills modification

---
+++ src/components/BackendSkills.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const BackendSkills = ({ skills }) => {
+    return (
+        <div className="skills-panel">
+            <h2 className="skills-panel-title">Backend Technologies</h2>
+            {skills.map(skill => (
+                <div className="skills__data" key={skill.id}>
+                    <div className="skills__names">
+                        <i className={`bx ${skill.icon_name} skills__icon`}></i>
+                        <span className="skills__name">{skill.skill_name}</span>
+                    </div>
+                    <div className="skills__bar skills__html"></div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default BackendSkills;
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,139 @@
-import React from 'react'
+// import React from 'react'
+
+// const skills_left = [
+//     {
+//         id: 1,
+//         icon_name: "bx bxl-spring-boot",
+//         skill_name: "Spring boot",
+//     },
+//     {
+//         id: 2,
+//         icon_name: "bx bxl-java",
+//         skill_name: "JAVA",
+//     },
+//     {
+//         id: 3,
+//         icon_name: "bxl-html5",
+//         skill_name: "HTML",
+//     },
+//     {
+//         id: 4,
+//         icon_name: "bxl-css3",
+//         skill_name: "CSS",
+//     },
+//     {
+//         id: 5,
+//         icon_name: "bxl-javascript",
+//         skill_name: "JAVASCRIPT",
+//     },
+//     {
+//         id: 6,
+//         icon_name: "bxs-data",
+//         skill_name: "MYSQL",
+//     },
+
+// ]
+
+// const skills_right = [
+//     {
+//         id: 1,
+//         icon_name: "bx bxl-mongodb",
+//         skill_name: "MongoDB",
+//     },
+//     {
+//         id: 2,
+//         icon_name: "bx bxl-git",
+//         skill_name: "GitHub",
+//     },
+//     {
+//         id: 3,
+//         icon_name: "bx bx-code",
+//         skill_name: "IntelliJ IDEA",
+//     },
+//     {
+//         id: 4,
+//         icon_name: "bx bx-data",
+//         skill_name: "Databases",
+//     },
+//     {
+//         id: 5,
+//         icon_name: "bx bxl-visual-studio",
+//         skill_name: "Visual Studio",
+//     },
+// ]
+
+
+// const Skills = () => {
+//     return (
+//         <section className="skills section" id="skills">
+//             <h2 className="section-title">Skills</h2>
+
+//             <div className="skills__container bd-grid">
+
+//                 <div>
+//                     {
+//                         skills_left.map((sl) => {
+//                             return (
+//                                 <div key={sl.id}>
+//                                     {/* <h2 className="skills__subtitle"></h2> */}
+//                                     {/* <p className="skills__text"></p> */}
+
+//                                     <div className="skills__data" key={sl.id}
+//                                         data-aos="zoom-in-left"
+//                                         data-aos-offset="100"
+//                                         data-aos-duration="1000">
+//                                         <div className="skills__names">
+//                                             <i className={`bx ${sl.icon_name} skills__icon`}></i>
+//                                             <span className="skills__name">{sl.skill_name}</span>
+//                                         </div>
+//                                         <div className="skills__bar skills__html"></div>
+//                                     </div>
+//                                 </div>
+//                             )
+//                         })
+//                     }
+//                 </div>
+//                 <div>
+//                     {
+//                         skills_right.map((sr) => {
+//                             return (
+//                                 <div key={sr.id}>
+//                                     {/* <h2 className="skills__subtitle"></h2> */}
+//                                     {/* <p className="skills__text"></p> */}
+
+//                                     <div className="skills__data"
+//                                         data-aos="zoom-in-right"
+//                                         data-aos-offset="100"
+//                                         data-aos-duration="1000" >
+//                                         <div className="skills__names">
+//                                             <i className={`bx ${sr.icon_name} skills__icon`}></i>
+//                                             <span className="skills__name">{sr.skill_name}</span>
+//                                         </div>
+//                                         <div className="skills__bar skills__html">
+//                                         </div>
+//                                     </div>
+//                                 </div>
+//                             )
+//                         })
+//                     }
+//                 </div>
+//             </div>
+//         </section>
+//     )
+// }
+
+// export default Skills
+
+
+
+
+
+//new code
+
+import React from 'react';
+import FrontendSkills from './FrontendSkills';
+import BackendSkills from './BackendSkills';
+import ToolsSkills from './ToolsSkills';
 
 const skills_left = [
     {
@@ -31,8 +166,7 @@ const skills_left = [
         icon_name: "bxs-data",
         skill_name: "MYSQL",
     },
-
-]
+];
 
 const skills_right = [
     {
@@ -60,66 +194,23 @@ const skills_right = [
         icon_name: "bx bxl-visual-studio",
         skill_name: "Visual Studio",
     },
-]
-
+];
 
 const Skills = () => {
+    const frontendSkills = skills_left.filter(skill => skill.id === 4 || skill.id === 5);
+    const backendSkills = skills_right.filter(skill => skill.id === 3 || skill.id === 2);
+    const toolsSkills = [...skills_left, ...skills_right].filter(skill => ![2, 3, 4, 5].includes(skill.id));
+
     return (
         <section className="skills section" id="skills">
             <h2 className="section-title">Skills</h2>
-
             <div className="skills__container bd-grid">
-
-                <div>
-                    {
-                        skills_left.map((sl) => {
-                            return (
-                                <div key={sl.id}>
-                                    {/* <h2 className="skills__subtitle"></h2> */}
-                                    {/* <p className="skills__text"></p> */}
-
-                                    <div className="skills__data" key={sl.id}
-                                        data-aos="zoom-in-left"
-                                        data-aos-offset="100"
-                                        data-aos-duration="1000">
-                                        <div className="skills__names">
-                                            <i className={`bx ${sl.icon_name} skills__icon`}></i>
-                                            <span className="skills__name">{sl.skill_name}</span>
-                                        </div>
-                                        <div className="skills__bar skills__html"></div>
-                                    </div>
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-                <div>
-                    {
-                        skills_right.map((sr) => {
-                            return (
-                                <div key={sr.id}>
-                                    {/* <h2 className="skills__subtitle"></h2> */}
-                                    {/* <p className="skills__text"></p> */}
-
-                                    <div className="skills__data"
-                                        data-aos="zoom-in-right"
-                                        data-aos-offset="100"
-                                        data-aos-duration="1000" >
-                                        <div className="skills__names">
-                                            <i className={`bx ${sr.icon_name} skills__icon`}></i>
-                                            <span className="skills__name">{sr.skill_name}</span>
-                                        </div>
-                                        <div className="skills__bar skills__html">
-                                        </div>
-                                    </div>
-                                </div>
-                            )
-                        })
-                    }
-                </div>
+                <FrontendSkills skills={frontendSkills} />
+                <BackendSkills skills={backendSkills} />
+                <ToolsSkills skills={toolsSkills} />
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default Skills
+export default Skills;
